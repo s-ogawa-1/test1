d6e65a7feeef045127de610f209e01a112f55029
Shoya Ogawa <s3-ogawa@bnba.jp>
2019-11-15 16:20:48 +0900

    add array calculate in add.py

--- add.py
+++ add.py
@@ -1,9 +1,14 @@
 #適当にコーディング
 
-import numpy
+import numpy as np
 
 def add(a, b):
     return a + b
 
 x = add(5, 15)
 print(x)
+
+array1 = np.array([[1, 2], [3, 4]])
+array2 = np.array([[10, 20], [30, 40]])
+array3 = add(array1, array2)
+print(array3)
